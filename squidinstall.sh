8ec1fb8527afc5db970fb0f969df0aae9e413d3e
melionor <78068969+melionor@users.noreply.github.com>
2021-01-27 07:45:23 +0100

    Update squidinstall.sh

--- squidinstall.sh
+++ squidinstall.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-apt-get install squid3
+yes | apt-get install squid3
 
 mv /etc/squid/squid.conf /etc/squid/squid.conf.old
-nano /etc/squid/squid.conf
 
-wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/melionor/squid/main/squidconf?token=ASTTZ2LRDLCRLYGSP7YYG7TACEEEE
+wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/melionor/squid/main/squidconf
 
 service squid restart
