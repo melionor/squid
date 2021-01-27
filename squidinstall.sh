1193b0c4061331279bcdca5f22b4d6ee4a18817b
melionor <78068969+melionor@users.noreply.github.com>
2021-01-27 08:15:10 +0100

    Update squidinstall.sh

--- squidinstall.sh
+++ squidinstall.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
 yes | apt-get install squid3
 
 mv /etc/squid/squid.conf /etc/squid/squid.conf.old
 
 wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/melionor/squid/main/squidconf
 
-user=$tr -cd 0-9 < /dev/urandom | head -c 12 ; echo ""
-pass=$tr -cd 0-9 < /dev/urandom | head -c 12 ; echo ""
+user=$(< /dev/urandom tr -cd a-zA-Z0-9 | head -c10)
+pass=$(< /dev/urandom tr -cd a-zA-Z0-9 | head -c10)
+ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
+echo port=51230
+echo user = $user
+echo pass = $pass
 
 /usr/bin/htpasswd -b -c /etc/squid/passwords $user $pass
 
