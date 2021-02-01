c75a155678aa6672713d4d1f3f7f0fbb4bbbffed
melionor <78068969+melionor@users.noreply.github.com>
2021-02-01 12:20:58 +0100

    Update squidinstall.sh

--- squidinstall.sh
+++ squidinstall.sh
@@ -6,6 +6,16 @@ mv /etc/squid/squid.conf /etc/squid/squid.conf.old
 
 wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/melionor/squid/main/squidconf
 
+echo "Mettre plusieurs proxy ? (oui ou non)"
+read nbproxy
+
+while nbproxy == oui
+do
+echo "Colle la conf stp: "
+read conf
+echo conf >> /etc/squid/squid.Conf
+echo "Je viens d'ajouter la conf dans le fichier !"
+
 user=$(< /dev/urandom tr -cd 0-9 | head -c10)
 pass=$(< /dev/urandom tr -cd 0-9 | head -c10)
 ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
