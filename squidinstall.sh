1383fbb03d3cc582a7a8350216f9a73c41abd9ed
melionor <78068969+melionor@users.noreply.github.com>
2021-03-07 20:25:46 +0100

    Update squidinstall.sh

--- squidinstall.sh
+++ squidinstall.sh
@@ -37,6 +37,6 @@ echo "Done"
 user=$(< /dev/urandom tr -cd 0-9 | head -c10)
 pass=$(< /dev/urandom tr -cd 0-9 | head -c10)
 ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
-echo port=51230
+echo port=3128
 echo user = $user
 echo pass = $pass
